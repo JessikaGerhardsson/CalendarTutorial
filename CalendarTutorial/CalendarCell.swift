d1ae486198076c86d5307eaa13f86d8dc4ab7adf
Jessika Gerhardsson <jessika_hej@hotmail.com>
2022-07-25 15:24:12 +0200

    initial commit

---
+++ CalendarTutorial/CalendarCell.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarCell.swift
+//  CalendarTutorial
+//
+//  Created by Jessika Gerhardsson on 2022-07-25.
+//
+
+import Foundation
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var dayOfMonth: UILabel!
+    
+}
---
+++ CalendarTutorial/CalendarHelper.swift
@@ -0,0 +1,64 @@
+//
+//  CalendarHelper.swift
+//  CalendarTutorial
+//
+//  Created by Jessika Gerhardsson on 2022-07-25.
+//
+
+import Foundation
+import UIKit
+
+class CalendarHelper {
+    
+    let calendar = Calendar.current
+    
+    func plusMonth(date: Date) -> Date {
+        return calendar.date(byAdding: .month, value: 1, to: date)!
+    }
+    
+    func minusMonth(date: Date) -> Date {
+        return calendar.date(byAdding: .month, value: -1, to: date)!
+    }
+    
+    //Input: 15 Jan 2021 -> Result: January
+    func monthString(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "LLLL"
+        return dateFormatter.string(from: date)
+    }
+    
+    //Input: 15 Jan 2021 -> Result: 2021
+    func yearString(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy"
+        return dateFormatter.string(from: date)
+    }
+    
+    //Input: 18 Jan 2021 -> Result: 31
+    //Returnar antalet dagar i månaden
+    func daysInMonth(date: Date) -> Int {
+        let range = calendar.range(of: .day, in: .month, for: date)!
+        return range.count
+    }
+    
+    //Returnar datumet
+    //Input: 2 Jan 2020 -> Result: 2
+    func dayOfMonth(date: Date) -> Int {
+        let components = calendar.dateComponents([.day], from: date)
+        return components.day!
+    }
+    
+    //Returnar den första i månaden
+    //Input: 15 Jan 2021 -> Result: 1 Jan 2021
+    func firstOfMonth(date: Date) -> Date {
+        let components = calendar.dateComponents([.year, .month], from: date)
+        return calendar.date(from: components)!
+    }
+    
+    //Returnar veckodagen
+    //Input: 1 Jan 2021 -> Result: 5(Friday)
+    func weekDay(date: Date) -> Int {
+        let components = calendar.dateComponents([.weekday], from: date)
+        return components.weekday! - 1
+    }
+}
